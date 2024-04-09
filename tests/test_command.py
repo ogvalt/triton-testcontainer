fa213bbd6c0df115d7dba87bcfc46271c3278efd
Oleksandr Popovych <ogvalt@gmail.com>
2024-04-09 18:42:36 +0300

    minor: add more options to TritonCommand class

--- tests/test_command.py
+++ tests/test_command.py
@@ -25,5 +25,28 @@ def test_command_correct():
         pattern = rf"(?<= )({escaped_substring})(?= )|(?<= ){escaped_substring}$"
 
         assert len(re.findall(pattern=pattern, string=cmd)) == 1
+
+def test_triton_command():
+    cmd = TritonCommand(
+        id="test",
+        exit_timeout_secs=10,
+        log_verbose=1,
+        log_info=True,
+        log_warning=True,
+        log_error=True,
+        model_store=["/workspace/models"],
+        model_repository=["/workspace/models"],
+        exit_on_error=True,
+        disable_auto_complete_config=True,
+        strict_readiness=True,
+        model_control_mode="explicit",
+        repository_pool_secs=10,
+        load_model="test",
+        model_load_thread_count=1,
+        model_load_retry_count=1,
+        model_namespacing=True,
+        allow_metrics=True,
+    )
+    print(cmd.build())
     
 
--- triton_testcontainer/command.py
+++ triton_testcontainer/command.py
@@ -2,8 +2,12 @@
 This module contains the class TritonCommand that implements generation of
 valid tritonserver cli command using Builder pattern.
 """
-from typing import Literal
+from typing import Literal, TypeAlias, TypeVar
 from pydantic import BaseModel, Field, ConfigDict
+
+
+# FlagType: TypeAlias = None | Literal[True] | Literal[1]
+FlagType = TypeVar("FlagType", None, Literal[True])
 
 class TritonCommand(BaseModel):
     """
@@ -14,28 +18,36 @@ class TritonCommand(BaseModel):
 
     _command: str = "tritonserver"
 
+    # Server
+
+    id: None | str = None
+    exit_timeout_secs: None | int = None
+
     # Logging
     log_verbose: None | int = None
-    log_info:    None | Literal[0, 1] = None
-    log_warning: None | Literal[0, 1] = None
-    log_error:   None | Literal[0, 1] = None
-    log_format:  None | str = None
-    log_file:    None | str = None
+    log_info:    None | bool = None
+    log_warning: None | bool = None
+    log_error:   None | bool = None
+    # log_format:  None | str = None
+    # log_file:    None | str = None
 
     # Model Repository
     model_store: None | list[str] = None
     model_repository: list[str] = Field(default_factory=lambda: ["/workspace"])
 
-    exit_on_error: None | Literal[0, 1] = None
-    # disable_auto_complete_config: None | Literal[True] = None
-    strict_readiness: None | Literal[0, 1] = None
+    exit_on_error: None | bool = None
+    disable_auto_complete_config: FlagType = None
+    strict_readiness: None | bool = None
+
     model_control_mode: Literal["none", "poll", "explicit"] = "none"
 
     repository_pool_secs: None | int = None
 
     load_model: None | Literal["*"] | str | list[str] = None
+
+    model_load_thread_count: None | int = None
     model_load_retry_count: None | int = None
-    # model_namespacing: None | Literal[True] = None
+    model_namespacing: None | bool = None
 
     # HTTP
     # allow_http: None | Literal[True] = None
@@ -43,16 +55,42 @@ class TritonCommand(BaseModel):
     # GRPS
     # allow_grpc: None | Literal[True] = None
 
+    # Sagemaker
+
+    # Vertex
+
+    # Metrics
+    allow_metrics: None | bool = None
+
+    # Tracing
+
+    # Backend
+
+    # Repository Agent
+
+    # Response Cache
+
+    # Rate Limiter
+
+    # Memory/Device Management
+
+
     def build(self):
         for fld, val in iter(self):
-            self._append_option(fld, val)
+            annotation = str(type(self).model_fields[fld].annotation)
+            self._append_option(fld, val, annotation)
         return self._command
     
-    def _append_option(self, name: str, value: str | list[str]):
+    def _append_option(self, name: str, value: str | list[str], annotation: str):
         _name = name.replace("_", "-")
 
         match value:
-            case str():
+            case bool():
+                if "FlagType" in annotation:
+                    option = f"--{_name}"
+                else:
+                    option = f"--{_name}={int(value)}"
+            case str() | int():
                 option = f"--{_name}={value}"
             case list():
                 option = " ".join([f"--{_name}={v}" for v in value])
