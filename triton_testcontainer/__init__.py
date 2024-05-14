bc89865ed604f7546465207281a2a476e07d2c6f
Oleksandr Popovych <ogvalt@gmail.com>
2024-05-14 16:04:35 +0300

    patch: add image builder to package interface

--- triton_testcontainer/__init__.py
+++ triton_testcontainer/__init__.py
@@ -2,3 +2,4 @@ from .triton import TritonContainer, VolumeMapping
 from .command import TritonCommand
 
 from .dockerfile_builder import DockerfileBuilder
+from .image_builder import ImageBuilder, BuildOptions, ContainerLimits
